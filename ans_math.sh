be7e683938ec7947a07a70ec6143b7913d74d959
Sabry Razick <sabryr@gmail.com>
2019-05-30 16:52:22 +0200

    with answers

--- ans_math.sh
+++ ans_math.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 i=0;
+p_c_sum=""
+p_c_sum_a=""
+date > out.txt
+date > out_ans.txt
+
 for num in $(shuf -i 0-99 -n $1) 
 do
+	c_sum=""
+	c_sum_a=""
+
 	i=$((i + 1))
 	operator="-"
 	if [ $(($i % 4)) == 0 ] || [ $(($i % 4)) == 5 ]
@@ -13,20 +21,34 @@ do
 
 	if [ $operator == "+" ]
 	then
-		echo $num "+" $num2;
+		c_sum="$num+$num2=___";
+		let ans=$num+$num2
+		c_sum_a="$num+$num2=$ans";
 	elif [ $operator == "-" ];
 	then
 		if [ "$num2" -gt "$num"  ]
         	then
-			echo $num2 "-" $num;
+			c_sum="$num2-$num=___";
+			let ans=$num2-$num
+			c_sum_a="$num2-$num=$ans";
 		else
-			echo $num "-" $num2;
+			c_sum="$num-$num2=___";
+			let ans=$num-$num2
+			c_sum_a="$num-$num2=$ans";
 		fi
 
 	else
 		echo "error"; 
 	fi
-
+	p_c_sum="$p_c_sum\t$c_sum"
+	p_c_sum_a="$p_c_sum_a\t$c_sum_a"
+	if [ $(($i % 3)) == 0 ]
+	then
+		echo -e $p_c_sum >> out.txt
+		echo -e $p_c_sum_a >> out_ans.txt
+		p_c_sum=""
+		p_c_sum_a=""
+	fi
 done
 
 
